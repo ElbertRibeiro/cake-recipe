ebff4d4380aadb7beb177e972cb9a737813702ed
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-05-21 17:47:55 -0300

    update struture of project

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes.js';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>app</Text>
-    </View>
+    <>
+      <Routes />>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
