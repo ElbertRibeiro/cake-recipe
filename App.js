e12846160697e0dae6df3a3922325727c886e29a
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-05-22 20:11:36 -0300

    status bar add

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { StatusBar, YellowBox } from 'react-native';
 
 import Routes from './src/routes.js';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
 
 export default function App() {
   return (
     <>
-      <Routes />>
+     <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+     <Routes />>
     </>
   );
 }
